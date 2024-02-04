32b0ed1216ebf508f750437d74b5665d0af62bc9
Aaron Ballagan <aaronbee2010@live.co.uk>
2024-02-04 20:07:48 +0000

    Showing Chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -17,7 +17,7 @@ class ChirpController extends Controller
     public function index(): Response {
         // return response("Hello World");
         return Inertia::render('Chirps/Index', [
-            //
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
         ]);
     }
 
